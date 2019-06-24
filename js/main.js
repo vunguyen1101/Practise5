d9366ea6d57387bd1a45ce2d697be6489120669f
ntvu <ntvu111@gmail.com>
2019-06-24 15:40:25 +0700

    Update Shop, Meal, BlogSingle  pages

--- js/main.js
+++ js/main.js
@@ -22,3 +22,75 @@ $(document).ready(function(){
       return false;
     });
   });
+
+  // scroll to section
+  $("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top;
+  
+    $("body, html").animate({
+      scrollTop: position
+    },1500);
+  });
+
+///Quantity
+  $('#add').click(function () {
+    if ($('#singleDish').val() < 100) {
+      $('#singleDish').val(+$('#singleDish').val() + 1);
+    }
+  });
+  $('#sub').click(function () {
+    if ($('#singleDish').val() > 0) {
+      if ($('#singleDish').val() > 0) $('#singleDish').val(+$('#singleDish').val() - 1);
+    }
+  });
+
+// Slick
+
+$(document).ready(function(){
+  $('.menu__bigposter--quote .container').slick({
+    nextArrow: false,
+    prevArrow: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.Meal__carousel .row').slick({
+    nextArrow: false,
+    prevArrow: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  
+    ]
+  });
+});
+
+	
